1ac37ae836de4f14497c75994bc60847b77ac0e4
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-11-04 23:43:01 +0530

    Modified the changes

--- public/admin.php
+++ public/admin.php
@@ -15,7 +15,7 @@
     <div class="header-top">
         <nav class="navbar navbar-default">
             <div class="container">
-                <a class="navbar-brand" href="#">LOGO</a>
+                <a class="navbar-brand" href="#">CMS</a>
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                         <span class="icon-bar"></span>
@@ -26,22 +26,6 @@
                 <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#">HOME</a></li>
-                        <li class="dropdown">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">NEWS <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Page 1-1</a></li>
-                                <li><a href="#">Page 1-2</a></li>
-                                <li><a href="#">Page 1-3</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">FEATURES <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Page 1-1</a></li>
-                                <li><a href="#">Page 1-2</a></li>
-                                <li><a href="#">Page 1-3</a></li>
-                            </ul>
-                        </li>
                         <li class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#">DEMOS<span class="caret"></span></a>
                             <ul class="dropdown-menu">
@@ -55,10 +39,7 @@
             </div>
         </nav>
         <div class="hero">
-            <h1>This is a Bootstrap header</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut tempor enim.</p>
-            <div class="btn btn-primary">Learn more about us</div>
-            <div class="btn btn-primary">Contact us</div>
+                
         </div>
     </div>
 
