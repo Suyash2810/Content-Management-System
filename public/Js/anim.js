6bb5d75d919827aed41b5f4eb34303e432671b79
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-11-07 00:05:01 +0530

    Added the scroll animation

--- public/Js/anim.js
+++ public/Js/anim.js
@@ -6,4 +6,14 @@ $(document).ready(function () {
             marginRight: '+=60px'
         }, 1000);
     }, 1000);
-})
+});
+
+$(document).ready(function () {
+
+    $("#arrow").on('click', function () {
+        var position = $(".up").offset().top;
+        $("HTML, BODY").animate({
+            scrollTop: position
+        }, 500);
+    });
+});
