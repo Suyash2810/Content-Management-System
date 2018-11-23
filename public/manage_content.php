e15cd6afadf9e03bcf401a371dbcd6deba6b9897
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-11-24 01:07:24 +0530

    Established the database connection

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,3 +1,23 @@
+<?php 
+        //Creating a database connection
+        $dbhost = 'localhost';
+        $dbuser = 'root';
+        $dbpass = 'secret';
+        $dbname = 'widget_corp';
+
+        $connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+        //Checking whether the connection has been established successfully.
+
+        if(mysqli_connect_errno())
+         {
+             die(
+                 "Database Connection has failed: " .
+                 mysqli_connect_error() . 
+                "( " . mysqli_connect_errno() . " )"
+             );
+         }
+?>
 <?php require_once('../Includes/functions.php'); ?>
 <?php include('../Includes/templates/header.php'); ?>
     <div class="container-fluid">
