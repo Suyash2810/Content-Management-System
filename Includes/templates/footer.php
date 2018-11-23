9823aadac79164498e18c0f4627c223bf22743a4
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-11-24 00:45:43 +0530

    Replaced the header and footer with reusable templates.

---
+++ Includes/templates/footer.php
@@ -0,0 +1,16 @@
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.2.0/anime.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="Js/anim.js"></script>
+    <script>
+        AOS.init({
+            once: true
+        });
+    </script>
+</body>
+
+</html>
