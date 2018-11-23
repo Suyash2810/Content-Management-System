06cc7a8a364c35bd511ec0f1f38ab527b943ee56
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-11-24 00:50:44 +0530

    Added an include for functions to be used

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,3 +1,4 @@
+<?php require_once('../Includes/functions.php'); ?>
 <?php include('../Includes/templates/header.php'); ?>
     <div class="container-fluid">
         <div class="well" id="main_well">
