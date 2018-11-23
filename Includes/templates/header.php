712d71d7ecf70105e0635b4de6514130b28ee960
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2018-11-24 00:40:11 +0530

    Added reusable header template

---
+++ Includes/templates/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles\styles.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+    <div class="header-top">
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <a class="navbar-brand" href="#">CMS</a>
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">HOME</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">DEMOS<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Page 1-1</a></li>
+                                <li><a href="#">Page 1-2</a></li>
+                                <li><a href="#">Page 1-3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="hero">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="display1" id="heading_cms">
+                        Content Management <br>
+                        System
+                    </h1>
+
+                    <h2 class="display2">
+                        Admin Page
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div id="arrow">
+            <a href="#" class="fa fa-arrow-down fa-2x" class="down"></a>
+        </div>
+    </div>
+    <br><br>
---
+++ public/admin.php
@@ -0,0 +1,64 @@
+<?php include('../Includes/templates/header.php'); ?>
+    <div class="container-fluid">
+        <div class="well" id="main_well">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <p class="up">
+                        <h1 class="display1" id="tech_heading" data-aos="zoom-in-up" data-aos-duration="1000">Tech
+                            Corporation</h1>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="well" id="inner" data-aos="fade-right" data-aos-duration="2000" data-aos-delay="100">
+                            <div class="col-md-12">
+                                <div id="center_anim">
+                                    <div id="cernter_anim_inner">Click Here.</div>
+                                </div>
+                            </div>
+                            <div id="hidden_info">
+                                <div>
+                                    Info
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="well" id="inner2" data-aos="fade-left" data-aos-duration="2000" data-aos-delay="200">
+                            <div class="col-md-8 col-md-offset-2">
+                                <h2>Admin Menu</h2>
+                                <h6 style="margin-top:5%;">Welcome to the admin area.</h6>
+                                <div id="list">
+                                    <ul>
+                                        <li><a href="./manage_content.html">Manage Website Content</a></li>
+                                        <li><a href="#">Manage Admin Users</a></li>
+                                        <li><a href="#">Logout</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.2.0/anime.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="Js/anim.js"></script>
+    <script>
+        AOS.init({
+            once: true
+        });
+    </script>
+</body>
+
+</html>
